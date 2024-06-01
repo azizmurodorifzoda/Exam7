d009dff216e051447ca24df8ffc3fd1cab4b0126
azizmurodorifzoda <azizmurodorifzoda55@gmail.com>
2024-06-01 19:52:26 +0500

    first commit

---
+++ task1.py
@@ -0,0 +1,9 @@
+# class User:
+#     def __init__(self, first, last, username, password):
+#         self.first=first
+#         self.last=last
+#         self.username=username
+#         self.password=password
+
+# class UserManager:
+    
